3f9d3be3dcab4fbc5129b4453adb230366203b29
Graham <graham.lim88@gmail.com>
2022-07-18 15:40:46 +0800

    rewriting npm packages for capstone app compatibility

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import { Router } from '@reach/router';
-// import { useSelector, useDispatch } from 'react-redux';
-
 import AssignUsers from './components/AssignUsers';
-// import SignIn from './components/SignIn/SignIn';
-// import SignUp from './components/SignUp/SignUp';
 import Preparation from './components/Preparation';
 import Sign from './components/Sign';
 import View from './components/View';
-// import Header from './components/Header';
-// import PasswordReset from './components/PasswordReset/PasswordReset';
 import Welcome from './components/Welcome';
 
-// import { auth, generateUserDocument } from './firebase/firebase';
 
 import './App.css';
 
 const App = () => {
-  // const user = useSelector(selectUser);
-  // const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   auth.onAuthStateChanged(async userAuth => {
-  //     if (userAuth) {
-  //       const user = await generateUserDocument(userAuth);
-  //       const { uid, displayName, email, photoURL } = user;
-  //       dispatch(setUser({ uid, displayName, email, photoURL }));
-  //     }
-  //   });
-  // }, [dispatch]);
 
   return (
     <div>
